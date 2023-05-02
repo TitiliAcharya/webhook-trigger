5fca7c5116aabbdb28272543c2934e12006aaf29
lucky <lucky@LAPTOP-JKGC4BSE>
2023-05-02 00:23:06 -0400

    webhook checking

--- src/Practice/Webhook.java
+++ src/Practice/Webhook.java
@@ -1,12 +1,13 @@
 package Practice;
 
-public class Webhook {
+public class Webhook{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(10*90);
 		System.out.println("Checking webhook trigger");
 		System.out.println("Checking trigger");
+		System.out.println("checking jenkins");
 	}
 
 }
